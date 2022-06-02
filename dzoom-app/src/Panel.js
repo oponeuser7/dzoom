e0870149f26bf97d5033f2cb8d9185bacc6d6e15
lavgin7 <oponeuser7@gmail.com>
2022-06-02 14:37:30 +0900

    import useRef

--- dzoom-app/src/Panel.js
+++ dzoom-app/src/Panel.js
@@ -1,15 +1,26 @@
-import './style.css'
-import axios
+import './style.css';
+import { useRef } from 'react';
+import axios from 'axios';
 
 const Panel = () => {
+
+  const x = useRef(null);
+  const y = useRef(null);
+  const width = useRef(null);
+  const height = useRef(null);
+  
+  const handleClick = () => {
+    return console.log(x.current.value); 
+  };
+    
   return (
     <div>
-      <div className="block"><label>X : <input type="number" id="x"/></label></div>
-      <div className="block"><label>Width : <input type="number" id="width"/></label></div>
+      <div className="block"><label>X : <input ref={x} type="number"/></label></div>
+      <div className="block"><label>Width : <input ref={width} type="number"/></label></div>
       <br/>
-      <div className="block"><label>Y : <input type="number" id="y"/></label></div>
-      <div className="block"><label>Height : <input type="number" id="height"/></label></div>
-      <div className="block"><button type="button" id="resolution-button">Resolution</button></div>
+      <div className="block"><label>Y : <input ref={y} type="number" id="y"/></label></div>
+      <div className="block"><label>Height : <input ref={height} type="number" id="height"/></label></div>
+      <div className="block"><button type="button" id="resolution-button" onClick={handleClick}>Resolution</button></div>
     </div>
   );
 }
