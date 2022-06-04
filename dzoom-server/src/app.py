28a0ddfa40ef12814081c3ae4f8ab74d76edd4da
lavgin7 <oponeuser7@gmail.com>
2022-06-04 12:04:59 +0900

    feat: uploading src image from server

--- dzoom-server/src/app.py
+++ dzoom-server/src/app.py
@@ -1,13 +1,19 @@
 import os
 from main import main
-from flask import Flask
+from flask import Flask, render_template
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/")
+@app.route('/')
 def hello():
     main()
     return "Done!"
+
+@app.route('/image')
+def image():
+    return render_template('image.html');
 
 if __name__ == '__main__':
     app.run()
