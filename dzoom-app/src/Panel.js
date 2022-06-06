0b6798b6b931de5007e084c3c135c4d2ca879ebe
lavgin7 <oponeuser7@gmail.com>
2022-06-06 23:37:02 +0900

    ready commit for test

--- dzoom-app/src/Panel.js
+++ dzoom-app/src/Panel.js
@@ -2,8 +2,23 @@ import './style.css';
 import { useState, useRef } from 'react';
 import axios from 'axios';
 
+const LoadingText = () => {
+  const [text, setText] = useState('Loading');
+
+  setInterval(() => {
+    setText(text+'.');
+    if(text.length>=10) setText('Loading');
+  }, 1000);
+
+  return (
+    <div className='block'>{text}</div>
+  );
+}
+
 const Panel = () => {
   const [source, setSource] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [flag, setFlag] = useState(false);
   const x = useRef(null);
   const y = useRef(null);
   const width = useRef(null);
@@ -12,12 +27,26 @@ const Panel = () => {
 
   const resolution = () => {
     console.log("Sending Request...");
-    axios.get('//127.0.0.1:5000', {})
+    setFlag(false);
+    setLoading(true);
+    axios.get('//127.0.0.1:5000',
+      {
+        params: {
+          x: x.current.value,
+          y: y.current.value,
+          width: width.current.value,
+          height: height.current.value
+        }
+      })
       .then((response) => {
-        setSource('http://127.0.0.1:5000/static/images/image_x2_SR.png');
+        setFlag(true);
+        setLoading(false);
         console.log("Done!");
+        alert('Done!');
       }).catch(() => {
+        setLoading(false);
         console.log("Error!");
+        alert('Error!');
       });
   };
 
@@ -34,10 +63,12 @@ const Panel = () => {
       <div id='panel'>
         <div className="block"><label>X : <input ref={x} type="number"/></label></div>
         <div className="block"><label>Width : <input ref={width} type="number"/></label></div>
+        {loading ? <LoadingText/> : ''}
         <br/>
         <div className="block"><label>Y : <input ref={y} type="number" id="y"/></label></div>
         <div className="block"><label>Height : <input ref={height} type="number" id="height"/></label></div>
         <div className="block"><button type="button" onClick={resolution}>Resolution</button></div>
+        {flag ? <a href='http://127.0.0.1:5000/static/images/image_x2.png' target='_blank'>Show Result</a> : ''}
         <div className="block"><input ref={file} type='file'/></div>
         <div className="block"><button type="button" onClick={uploadImage}>Upload File</button></div>
       </div>
--- dzoom-server/src/app.py
+++ dzoom-server/src/app.py
@@ -1,14 +1,23 @@
 import os
+import cv2
 from main import main
-from flask import Flask, render_template
+from flask import Flask, request
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def hello():
-    main()
+    img = cv2.imread('../experiment/test/results-Demo/image_x2_SR.png')
+    if img is None: main()
+    params = request.args
+    x1 = int(params.get('x'))
+    x2 = x1+int(params.get('width'))
+    y1 = int(params.get('y'))
+    y2 = y1+int(params.get('height'))
+    img = img[y1:y2, x1:x2]
+    cv2.imwrite('static/images/image_x2.png', img)
     return "Done!"
 
 if __name__ == '__main__':
