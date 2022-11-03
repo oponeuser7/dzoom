54ef8af318c598026d82b92339236eb9c55f307e
oponeuser7 <oponeuser7@gmail.com>
2022-11-03 21:16:45 +0900

    feat: file save

--- dzoom-server/src/app.py
+++ dzoom-server/src/app.py
@@ -7,17 +7,25 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['POST'])
 def hello():
-    img = cv2.imread('../experiment/test/results-Demo/image_x2_SR.png')
-    if img is None: main()
-    params = request.args
-    x1 = int(params.get('x'))
-    x2 = x1+int(params.get('width'))
-    y1 = int(params.get('y'))
-    y2 = y1+int(params.get('height'))
+    file = request.files['file']
+    file_name = request.form['fileName']
+    file_name = file_name[:len(file_name)-4]
+    file.save('./static/images/'+file_name+'.png')
+    img = cv2.imread('../experiment/test/results-Demo/'+file_name+'_x2_SR.png')
+    if img is None: 
+        main()
+        img = cv2.imread('../experiment/test/results-Demo/'+file_name+'_x2_SR.png')
+    params = request.form
+    x1 = int(params['x'])
+    x2 = x1+int(params['width'])
+    y1 = int(params['y'])
+    y2 = y1+int(params['height'])
     img = img[y1:y2, x1:x2]
-    cv2.imwrite('static/images/image_x2.png', img)
+    cv2.imwrite('static/results/'+file_name+'_x2_SR.png', img)
+    for file in os.scandir('./static/images'):
+        os.remove(file.path)
     return "Done!"
 
 if __name__ == '__main__':
